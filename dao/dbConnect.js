11fa05c047dc59b7551565dff33f160e8e120d73
BaktybekSagidolla <145594221+BaktybekSagidolla@users.noreply.github.com>
2024-02-16 21:28:45 +0600

    Add files via upload

---
+++ dao/dbConnect.js
@@ -0,0 +1,12 @@
+// Example dbConnect.js
+module.exports = {
+    connect: function() {
+        // Your database connection logic here
+    },
+    selectFun: function(client, username, callback) {
+        // Your select logic here
+    },
+    insertFun: function(client, username, password, chainadd, callback) {
+        // Your insert logic here
+    }
+};
